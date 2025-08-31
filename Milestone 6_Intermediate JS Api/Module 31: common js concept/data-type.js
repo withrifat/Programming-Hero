8f7ea4fc39f2267c1defa6b7c495eb05cabe4906
withrifat <mdrifathassain97880@gmail.com>
2025-08-31 23:44:22 +0600

    add module 31 file

---
+++ Milestone 6_Intermediate JS Api/Module 31: common js concept/data-type.js
@@ -0,0 +1,22 @@
+// JS is a dynamic typed programming language
+let data = 4;
+data = 'tormuj';
+data = false;
+data = { name: 'vaangi', price: 40 }
+data = [445]
+data = undefined
+data = null
+console.log(typeof data);
+
+// js: primitive data type
+const price = 50;
+const name = 'bodrul';
+const isPoor = false;
+let isKnown = undefined; // usually you should not set value to undefined
+let isLocal;
+const merit = null;
+console.log(isKnown, isLocal)
+
+// non-primitive data type
+const student = { name: 'badal', location: 'bristi goli', subject: 'botany' }
+const friends = ['tom', 'bob', 'jack', 'rock']
