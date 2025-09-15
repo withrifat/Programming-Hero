65fc45019ae7083ca9c321ba23276b16f462a914
withrifat <mdrifathassain97880@gmail.com>
2025-09-15 23:51:57 +0600

    add note for different ways to handle events in react

--- Milestone 7_react/Module 37 react core2/src/App.jsx
+++ Milestone 7_react/Module 37 react core2/src/App.jsx
@@ -3,10 +3,21 @@ import './App.css'
 
 function App() {
 
-
+  function handleClick(){
+    alert('I am Clicked . ')
+  }
+  const handleAdd5=(num)=>{
+    let val = num + 5;
+    alert("value is "+ val);
+  }
+  const clickMeBtn = () => alert("someone clicked me ");
   return (
-    <>
-
+    <>  
+        <h3>Hi </h3>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={clickMeBtn}>Click me 1</button>
+        <button onClick={() => alert("I am clicked")}>Click me 2</button>
+        <button onClick={()=>handleAdd5(10)}>Click me 5</button>
     </>
   )
 }
