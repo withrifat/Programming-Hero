a16b149c42b7dd4acff423c055e5f772084de462
withrifat <mdrifathassain97880@gmail.com>
2025-09-21 02:39:54 +0600

    besic setup and dummy data make a json file

--- Milestone 7_react/Module 39 module and data storge integration/src/App.jsx
+++ Milestone 7_react/Module 39 module and data storge integration/src/App.jsx
@@ -2,10 +2,19 @@
 import './index.css'
 
 function App() {
+    const bottles = [
+    { id: 1, name: 'Pink Nike Bottle', price: 250, color: 'pink' },
+    { id: 2, name: 'Pink Nike Bottle', price: 250, color: 'pink' },
+    { id: 3, name: 'Pink Nike Bottle', price: 250, color: 'pink' },
+    { id: 4, name: 'Pink Nike Bottle', price: 250, color: 'pink' },
+  ]
 
   return (
     <>
-     
+    <div className="mx-auto container mt-2 ">
+     <h2 className="text-center ">Buy Awesome Water Bottle </h2>
+
+    </div>
     </>
   )
 }
