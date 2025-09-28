d0bca29746b9e2d1ae3b3f61f7852ceae07f2462
withrifat <mdrifathassain97880@gmail.com>
2025-09-28 12:30:09 +0600

    add hole note module 42 tailsind axios owsome component

--- Milestone 8_react router state management/Module 42_tailwind axios awsome component/src/App.jsx
+++ Milestone 8_react router state management/Module 42_tailwind axios awsome component/src/App.jsx
@@ -1,25 +1,38 @@
-// import { X } from "lucide";
-// import { use } from "react"6
-import GoogleMap from "./components/googleMap/GoogleMap";
-import Menu from "./components/Menu/Menu";
+import { Suspense } from 'react'
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
+import PricingOptions from './components/PricingOptions/PricingOptions'
+import ResultsChart from './components/ResultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
 
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
+const marksPromise = axios.get('marksData.json');
 
 function App() {
-  // const [open, setOpen] = use(false);
 
   return (
-    
     <>
-    <h2 className="text-white">This is h2 hallow</h2>
-      {/* <nav className="flex justify-baseline mx-10">
-          <span className="flex" onClick={()=> setOpen(!open)}>
-            {open?
-              <X className='md:hidden'></X>:
-              <Menu></Menu>
-            }
-          </span>
-      </nav> */}
-      <GoogleMap></GoogleMap>
+
+      <header>
+        <NavBar></NavBar>
+        {/* <DaisyNav></DaisyNav> */}
+      </header>
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+        </Suspense>
+
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <MarksChart marksPromise={marksPromise}></MarksChart>
+        </Suspense>
+
+        <ResultsChart></ResultsChart>
+
+      </main>
+
     </>
   )
 }
