9d4da58ef28961c88fa0929902f677754bb343b9
withrifat <mdrifathassain97880@gmail.com>
2025-09-28 16:59:55 +0600

    add mileston e7 consapcual

--- Computer Fundamenntals m1/Milestone 7 react project support/src/App.jsx
+++ Computer Fundamenntals m1/Milestone 7 react project support/src/App.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/footer/Footer';
 
+const fetchIssues = async () => {
+  const result = await fetch("/data.json");
+  return result.json();
+};
+
 const App = () => {
+    const fetchPromise = fetchIssues();
   return (
     <>
       <Navbar></Navbar>
+      <Suspense fallback={<Loading />}>
+        <IssuesManagement fetchPromise={fetchPromise} />
+      </Suspense>
+
       <Footer></Footer>
     </>
   );
