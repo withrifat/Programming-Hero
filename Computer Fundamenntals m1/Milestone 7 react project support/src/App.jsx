28569c6f2c919df2893ef32732bdbb263ec5fd76
withrifat <mdrifathassain97880@gmail.com>
2025-09-28 13:03:26 +0600

    add mileston 7 react project support

---
+++ Computer Fundamenntals m1/Milestone 7 react project support/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/footer/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
