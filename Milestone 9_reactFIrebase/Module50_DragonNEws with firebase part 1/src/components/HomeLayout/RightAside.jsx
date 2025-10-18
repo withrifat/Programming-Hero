f7285d2ef9da6eec4b30b829814874ce54333306
withrifat <mdrifathassain97880@gmail.com>
2025-10-18 15:46:38 +0600

    add first part note for firebase dragon news

--- Milestone 9_reactFIrebase/Module50_DragonNEws with firebase part 1/src/components/HomeLayout/RightAside.jsx
+++ Milestone 9_reactFIrebase/Module50_DragonNEws with firebase part 1/src/components/HomeLayout/RightAside.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
+import FindUs from './FindUs';
+import Qzone from './Qzone';
 
 const RightAside = () => {
     return (
-        <div>
+        <div className='space-y-8'>
             <SocialLogin></SocialLogin>
+            <FindUs></FindUs>
+            <Qzone></Qzone>
         </div>
     );
 };
--- Milestone 9_reactFIrebase/Module50_DragonNEws with firebase part 1/src/pages/Home.jsx
+++ Milestone 9_reactFIrebase/Module50_DragonNEws with firebase part 1/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
         <div>
-            home component
+            <Navigate to="/category/0"></Navigate>
         </div>
     );
 };
