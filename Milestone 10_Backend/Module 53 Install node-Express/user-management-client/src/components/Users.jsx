055f3de424f5897fdf09fa683acf4422ff73b9c0
withrifat <mdrifathassain97880@gmail.com>
2025-10-30 12:07:55 +0600

    add module 54

--- Milestone 10_Backend/Module 53 Install node-Express/user-management-client/src/components/Users.jsx
+++ Milestone 10_Backend/Module 53 Install node-Express/user-management-client/src/components/Users.jsx
@@ -1,28 +1,59 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
-const Users = ({usersPromise}) => {
-    const users = use(usersPromise)
-    console.log(users);
-    
-    return (
-        <div>
-            <div className="">
-                <form>
-                    <h3>Add a user</h3>
-                    <input type="text" />
-                    <br />
-                    <input type="email" name="" id="" />
-                    <br />
-                    <button>Add user</button>
-                </form>
-            </div>
-            <div className="">
-                {
-                    users.map(user => <p key={user.id}>{user.name} Email: {user.email}</p>)
-                }
-            </div>
-        </div>
-    );
+const Users = ({ usersPromise }) => {
+  const inisialUser = use(usersPromise);
+  const [users, setUsers] = useState(inisialUser);
+  console.log(users);
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const newUser = { name, email };
+
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('after post', data);
+        const newUsers = [...users, data]; 
+        setUsers(newUsers);
+        // e.target.reset(); 
+      })
+      .catch(err => console.error('Error:', err));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddUser}>
+        <h3>Add a user</h3>
+        <input name='name' type='text' placeholder='Name' required />
+        <br />
+        <input name='email' type='email' placeholder='Email' required />
+        <br />
+        <button type='submit'>Add user</button>
+      </form>
+
+      <div>
+        {users.map(user => (
+          <p key={user.id}>{user.name} — Email: {user.email}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
+
+
+
+// request object send to the server 
+//  mention method post 
+//  using fetch method post 
+//  mention heade: json type data 
--- Milestone 10_Backend/Module 53 Install node-Express/users-management-server/index.js
+++ Milestone 10_Backend/Module 53 Install node-Express/users-management-server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000; //if not port asagin then use 3000
 
 app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('users server is loading.. ');
@@ -18,6 +19,13 @@ const users = [
 app.get('/users', (req, res) => {
   res.send(users);
 });
+app.post('/users', (req, res)=>{
+    console.log("Post method called", req.body)
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser);
+})
 
 app.listen(port, () => {
   console.log(`users server started on port: ${port} `);
