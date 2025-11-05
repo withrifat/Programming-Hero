e51b0732a1415478f48d992b9d15387759567c99
withrifat <mdrifathassain97880@gmail.com>
2025-11-05 15:22:55 +0600

    add mongo db connection

---
+++ Consapcual /Milestone10: Backend project part 1/readme.md
@@ -0,0 +1,10 @@
+
+First Install Express Js
+Second We need to Setup express and Install Nodemon pkg for live backend code change 
+And We need to install Cors for Resource Sharing => we can share recourses by origin 
+
+
+## Mongo DB
+### Database Data Collect With
+1. find 
+2. findOne --> for particular one data it's called Document according to MongoDB
--- Consapcual /Milestone10: Backend project part 1/server/index.js
+++ Consapcual /Milestone10: Backend project part 1/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = 3000
 
@@ -9,6 +10,35 @@ app.use(cors({
 }))
 //  when data come we give permission with this 
 app.use(express.json())
+// mongo db part 
+const uri = "mongodb+srv://devrifathossain:t1Yn8xnDa57LtdBk@test.lr2ztxa.mongodb.net/?appName=Test";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('model-db');
+    const modelCollection = db.collection('models');
+
+    app.get('/models', async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
+
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
